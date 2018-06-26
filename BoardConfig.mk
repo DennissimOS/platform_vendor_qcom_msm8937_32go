c188bf017c63386f5846cbdbb89ee95bab4fee53
Shibin George <shibing@codeaurora.org>
2018-06-26 14:54:47 +0530

    msm8937_32go: Don't strip classes.dex from APKs on userdebug builds
    Change-Id: Id433766b5293dd6f98a37db591d809e11a47180c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,6 +224,10 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 ifeq ($(HOST_OS),linux)
     ifeq ($(WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY),)
         WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+        ifneq ($(TARGET_BUILD_VARIANT),user)
+            # Retain classes.dex in APK's for non-user builds
+            DEX_PREOPT_DEFAULT := nostripping
+        endif
     endif
 endif
 
